1cc42ceac1d5f0e59af856d083870b527bdee7ac
IvanDolomanji <156667166+IvanDolomanji@users.noreply.github.com>
2024-10-22 14:34:08 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Дано целое число n, указывающее номер дня недели от 1 до 7.
+            //По указанному номеру n вывести название соответствующего дня недели.
+            //Console.Write("Номер дня недели=");
+            //int day = int.Parse(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine("Понедельник"); break;
+            //    case 2: Console.WriteLine("Вторник"); break;
+            //    case 3: Console.WriteLine("Среда"); break;
+            //    case 4: Console.WriteLine("Четверг"); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суббота"); break;
+            //    case 7: Console.WriteLine("Воскресенье"); break;
+            //    default:Console.WriteLine(" "); break;
+            //        
+
+            //}
+            //}
+
+
+            //catch
+            //{
+            //}
+
+            //По номеру месяца вывести название времени года.
+            //    try 
+            //{ 
+
+            //    Console.Write("Номер месяца=");
+            //    int month = int.Parse(Console.ReadLine());
+            //    switch (month)
+            //    {
+            //        case 1: Console.WriteLine("Зима"); break;
+            //        case 2: Console.WriteLine("Зима"); break;
+            //        case 3: Console.WriteLine("Весна"); break;
+            //        case 4: Console.WriteLine("Весна"); break;
+            //        case 5: Console.WriteLine("Весна"); break;
+            //        case 6: Console.WriteLine("Лето"); break;
+            //        case 7: Console.WriteLine("Лето"); break;
+            //        case 8: Console.WriteLine("Лето"); break;
+            //        case 9: Console.WriteLine("Осень"); break;
+            //        case 10: Console.WriteLine("Осень"); break;
+            //        case 11: Console.WriteLine("Осень"); break;
+            //        case 12: Console.WriteLine("Зима"); break;
+            //        default: Console.WriteLine(" "); break;
+
+            //    }
+            //        Console.Read();
+
+            //    }      
+            //    catch { }
+
+            //Дан признак транспортного средства:
+            //a – автомобиль,
+            //в – велосипед,
+            //м - мотоцикл,
+            //с – самолет,
+            //п – поезд.
+            //Вывести на экран максимальную скорость транспортного средства в зависимости от введенного признака.
+            //try
+            //{
+
+            //    Console.Write("Введите признак=");
+            //    char Ven = char.Parse(Console.ReadLine());
+            //    switch (Ven)
+            //    {
+            //        case 'а': Console.WriteLine("330 км/ч"); break;
+            //        case 'в': Console.WriteLine("35 км/ч"); break;
+            //        case 'м': Console.WriteLine("299 км/ч"); break;
+            //        case 'с': Console.WriteLine("900 км/ч"); break;
+            //        case 'п': Console.WriteLine("600 км/ч"); break;
+
+            //        default: Console.WriteLine(" "); break;
+
+            //    }
+            //    Console.Read();
+
+            //}
+            //catch { }
+
+            //Вывести название предмета по введенной первой букве: ф – физика, м – математика, и – история, г – география, б – биология.
+            try
+            {
+
+                Console.Write("Введите признак=");
+                char Ven = char.Parse(Console.ReadLine());
+                switch (Ven)
+                {
+                    case 'ф': Console.WriteLine("Физика"); break;
+                    case 'м': Console.WriteLine("Математика"); break;
+                    case 'и': Console.WriteLine("История"); break;
+                    case 'г': Console.WriteLine("География"); break;
+                    case 'б': Console.WriteLine("Биология"); break;
+
+                    default: Console.WriteLine(" "); break;
+
+                }
+                Console.Read();
+
+            }
+            catch { }
+
+
+
+
+
+
+
+        }
+    }
+}
+            
+
+        
+    
+
